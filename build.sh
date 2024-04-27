147f1b5804c39503c6549bd3d8127163393f269e
Padevend <151115861+Padevend@users.noreply.github.com>
2024-04-27 11:53:01 +0100

    build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
+
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --no-input
